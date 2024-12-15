df86be17ea2179f829e8a3eb7908869c9c4634f5
dhimantks <165192040+dhimantks@users.noreply.github.com>
2024-12-15 20:17:44 +0530

    Update autolysis.py

--- autolysis.py
+++ autolysis.py
@@ -1,13 +1,13 @@
 # /// script
 # requires-python = ">=3.9"
 # dependencies = [
-#    "markdown",
-#    "openai",
-#    "pandas",
-#    "scikit-learn",
-#    "seaborn",
-#    "tabulate",
-#    "tenacity",
+# "markdown>=3.7",
+# "openai==0.28",
+# "pandas>=2.2.3",
+# "scikit-learn>=1.6.0",
+# "seaborn>=0.13.2",
+# "tabulate>=0.9.0",
+# "tenacity>=9.0.0",
 # ]
 # ///
 
